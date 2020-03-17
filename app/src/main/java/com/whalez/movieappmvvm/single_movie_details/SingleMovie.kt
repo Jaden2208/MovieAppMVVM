ac122447c82f2ec5076fee03e25b1d37e5461e62
ryu22 <ryu2208@naver.com>
2020-03-17 19:54:13 +0900

    Initial commit

---
+++ app/src/main/java/com/whalez/movieappmvvm/single_movie_details/SingleMovie.kt
@@ -0,0 +1,13 @@
+package com.whalez.movieappmvvm.single_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.whalez.movieappmvvm.R
+
+class SingleMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
